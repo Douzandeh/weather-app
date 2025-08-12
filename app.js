d0edd30f2bc63b1a142e7c5e7c9aae1197b263c8
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-08-12 04:37:31 +0330

    Gat base URL

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hi");
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "7c6e06ad070793922cbb2be0343d16f8";
+
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
+const getCurrentWeatherByName = async (city) => {
+  const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+
+const searchHandler = async () => {
+  const cityName = searchInput.value;
+  if (!cityName) {
+    alert("Please enter City name!");
+  }
+
+  const currentData = await getCurrentWeatherByName(cityName);
+  console.log(currentData);
+};
+
+searchButton.addEventListener("click", searchHandler);
